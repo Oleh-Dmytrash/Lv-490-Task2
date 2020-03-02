a24756fbe7ecfd5294451d987c8f9768575650db
Danylo Sadivnychyi <Danylotv43@gmail.com>
2020-03-02 15:32:09 +0200

    Refactored code to meet the code convention

--- Project1/src/AnyType.h
+++ Project1/src/AnyType.h
@@ -7,15 +7,15 @@
 
 class AnyType
 {
-	std::string type;
-	std::shared_ptr<void> data;
+	std::string m_type;
+	std::shared_ptr<void> m_data;
 public:
 	template<typename T>
 	AnyType(T value)
 	{
 		static_assert(std::is_fundamental<T>::value, "The class only supports fundamental types");
-		type = typeid(value).name(); 
-		data = std::make_shared<T>(value);
+		m_type = typeid(value).name(); 
+		m_data = std::make_shared<T>(value);
 	}
 
 
@@ -23,12 +23,12 @@ public:
 	AnyType operator=(T value) 
 	{
 		static_assert(std::is_fundamental<T>::value, "The class only supports fundamental types");
-		type = typeid(value).name();
-		data = std::make_shared<T>(value);
+		m_type = typeid(value).name();
+		m_data = std::make_shared<T>(value);
 		return *this;
 	}
 	
-	std::string Type()const { return type; }
+	std::string Type()const { return m_type; }
 	void Swap(AnyType& other)
 	{
 		std::swap(*this, other);
@@ -36,44 +36,44 @@ public:
 
 	size_t GetTypeSize() const
 	{
-		if (type == "bool") return sizeof(bool);
-		if (type == "char") return sizeof(char);
-		if (type == "int") return sizeof(int);
-		if (type == "float") return sizeof(float);
-		if (type == "double") return sizeof(double);
+		if (m_type == "bool") return sizeof(bool);
+		if (m_type == "char") return sizeof(char);
+		if (m_type == "int") return sizeof(int);
+		if (m_type == "float") return sizeof(float);
+		if (m_type == "double") return sizeof(double);
 	}
 	
 	bool ToBool()
 	{
-		if (type == "bool") return *(bool*)data.get();
+		if (m_type == "bool") return *(bool*)m_data.get();
 		throw std::runtime_error("Can not convert to bool");
 	}
 	char ToChar()
 	{
-		if (type == "bool" || type == "char") return *(char*)data.get();
+		if (m_type == "bool" || m_type == "char") return *(char*)m_data.get();
 		throw std::runtime_error("Can not convert to char");
 	}
 	int ToInt()
 	{
-		if (type == "bool" || type == "char" || type == "int") return *(int*)data.get();
+		if (m_type == "bool" || m_type == "char" || m_type == "int") return *(int*)m_data.get();
 		throw std::runtime_error("Can not convert to int");
 	}
 	float ToFloat()
 	{
 		float return_value = 0;
-		if (type == "float" ||
-			type == "int" ||
-			type == "char" ||
-			type == "bool")
+		if (m_type == "float" ||
+			m_type == "int" ||
+			m_type == "char" ||
+			m_type == "bool")
 		{
-			return_value = *(float*)data.get();
+			return_value = *(float*)m_data.get();
 			return return_value;
 		}
 		throw std::runtime_error("Can not convert to float");
 	}
 	double ToDouble()
 	{
-		double return_value  = *(double*)data.get();
+		double return_value  = *(double*)m_data.get();
 		return return_value;
 	}
 
